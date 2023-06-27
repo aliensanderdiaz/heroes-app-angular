2dbbc32adee730c09197eb9b52298892daec792b
Alexander Diaz <aliensanderdiaz@gmail.com>
2023-06-27 07:19:43 -0500

    heroes

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interfaces';
 import { environments } from 'src/environments/environments';
 
@@ -17,5 +17,16 @@ export class HeroesService {
 
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`)
+  }
+
+  getHeroeById(id: string): Observable<Hero | undefined> {
+    return this.http.get<Hero>(`${ this.baseUrl }/heroes/${ id }`)
+      .pipe(
+        catchError( error => of(undefined))
+      )
+  }
+
+  getSuggestions(query: string): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${ this.baseUrl }/heroes?q=${ query }&limit=6`)
   }
 }
